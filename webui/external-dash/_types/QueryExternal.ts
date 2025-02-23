b13d534b1c23ba65161cd9dfbc0e545485b23e4f
lovesworking <austinlovesworking@gmail.com>
2025-02-23 11:08:50 -0700

    remove external dash

--- webui/external-dash/_types/QueryExternal.ts
+++
@@ -1,5 +0,0 @@
-import { Query } from "@tanstack/react-query";
-export interface ExtendedQuery extends Query {
-  observersCount?: number; // Add the observersCount property to the Query
-  isQueryStale?: boolean; // isStale()
-}
